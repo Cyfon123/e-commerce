481ce9a019307c8c6230c5e08df69aad2d575197
Cyfon123 <dg739390@gmail.com>
2024-10-24 20:03:09 +0530

    Home Page Step 1

---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,12 @@
+export const mainCarouselData=[
+    {
+        image:"https://virtualbackgrounds.site/wp-content/uploads/2021/02/womens-clothing-store.jpg"
+    },
+    {
+        image:"https://cdn.pixabay.com/photo/2021/11/15/05/25/boutique-6796399_1280.jpg"
+    },
+    {
+        image:"https://virtualbackgrounds.site/wp-content/uploads/2021/02/womens-clothing-store.jpg"
+    }
+    
+]
